731a97ae459289014ed92dd6a364c7f001a58b6d
zachtschetter <zachary.tschetter@usd.edu>
2024-09-05 18:48:40 -0500

    working

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('javascript--spidermonkey--run-button.run', () => {
+    // Get the active text editor
+    const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "javascript--spidermonkey--run-button" is now active!');
+    if (editor) {
+      const document = editor.document;
+      const filePath = document.fileName;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('javascript--spidermonkey--run-button.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from JavaScript (SpiderMonkey) Run Button!');
-	});
+      // Get the current terminal or create a new one if none exists
+      let terminal = vscode.window.activeTerminal;
+      if (!terminal) {
+        terminal = vscode.window.createTerminal(`Run: ${filePath}`);
+      }
 
-	context.subscriptions.push(disposable);
+      // Focus on the terminal and run the file
+      terminal.show();
+      terminal.sendText(`js "${filePath}"`);  // For example, if you are running *.js files
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
--- src/test.js
+++ src/test.js
@@ -1,3 +1,5 @@
 
 
 print('hello')
+const s = readline()
+print(s)

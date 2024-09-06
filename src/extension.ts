b406caf374ea402a838cceaadb7b8ee1a85f2314
zachtschetter <zachary.tschetter@usd.edu>
2024-09-06 14:46:24 -0500

    added delay when no terminal open

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,34 @@
-import * as vscode from 'vscode';
+    import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('javascript--spidermonkey--run-button.run', () => {
-    // Get the active text editor
-    const editor = vscode.window.activeTextEditor;
+    export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('javascript--spidermonkey--run-button.run', () => {
+        // Get the active text editor
+        const editor = vscode.window.activeTextEditor;
 
-    if (editor) {
-      const document = editor.document;
-      const filePath = document.fileName;
+        if (editor) {
+        const document = editor.document;
+        const filePath = document.fileName;
 
-      // Get the current terminal or create a new one if none exists
-      let terminal = vscode.window.activeTerminal;
-      if (!terminal) {
-        terminal = vscode.window.createTerminal(`Run: ${filePath}`);
-      }
+        // Get the current terminal or create a new one if none exists
+        let terminal = vscode.window.activeTerminal;
+        if (!terminal) {
+            terminal = vscode.window.createTerminal(`Run: ${filePath}`);
+            terminal.show();
+            setTimeout(() => {
+                terminal!.sendText(`js "${filePath}"`);  // Assuming 'js' is the command for running SpiderMonkey
+            }, 300);  // Delay in milliseconds (adjust if needed)
+        }else{
+            terminal.show();
+            terminal.sendText(`js "${filePath}"`);  // For example, if you are running *.js files
+        }
 
-      // Focus on the terminal and run the file
-      terminal.show();
-      terminal.sendText(`js "${filePath}"`);  // For example, if you are running *.js files
+        // Focus on the terminal and run the file
+        
+        
+        }
+    });
+
+    context.subscriptions.push(disposable);
     }
-  });
 
-  context.subscriptions.push(disposable);
-}
-
-export function deactivate() {}
+    export function deactivate() {}

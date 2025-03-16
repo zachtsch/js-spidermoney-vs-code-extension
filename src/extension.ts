7411f289b6a6401b9217d3e811c156b1138e6204
zachtschetter <zachary.tschetter@usd.edu>
2025-03-16 14:50:46 -0500

    ts

--- src/extension.ts
+++ src/extension.ts
@@ -136,6 +136,29 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    let disposable2 = vscode.commands.registerCommand('javascript--spidermonkey--run-button.tsnode', () => {
+        // Get the active text editor
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+            const document = editor.document;
+            const filePath = document.fileName;
+
+            // Get the current terminal or create a new one if none exists
+            let terminal = vscode.window.activeTerminal;
+            if (!terminal) {
+                terminal = vscode.window.createTerminal(`Run: ${filePath}`);
+                terminal.show();
+                //setTimeout(() => {
+                terminal!.sendText(`clear\r\nts-node "${filePath}"`);  // Assuming 'js' is the command for running SpiderMonkey
+                //}, 500);  // Delay in milliseconds (adjust if needed)
+            }else{
+                terminal.show();
+                terminal.sendText(`ts-node "${filePath}"`);  // For example, if you are running *.js files
+            }
+        }
+    });
+
 
     // Focus on the terminal and run the file
     let dlAndPath = vscode.commands.registerCommand('javascript--spidermonkey--run-button.install', async () => {
@@ -159,6 +182,7 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
     context.subscriptions.push(dlAndPath);
         
 }
